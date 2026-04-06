e5a89609c1652dc0071453e0cb20d8d0c1b79d4a
felix-balbin <2023_felix.balbin@iticbcn.cat>
2026-04-06 12:04:08 +0200

    audio

--- vr2_activitat_lliure/Assets/Scripts/ZombieScripts/ZombieStateManager.cs
+++ vr2_activitat_lliure/Assets/Scripts/ZombieScripts/ZombieStateManager.cs
@@ -26,10 +26,16 @@ public class ZombieStateManager : MonoBehaviour
     public Material matYellow;
     public Material matRed;
 
+    public AudioClip[] audioClipArray;
+    public AudioClip zombieHitClip;
+    public AudioSource audioSource;
+    public AudioSource audioHitSource;
+
     private void Awake()
     {
         ctx = GetComponent<ZombieAIContext>();
         rendererMesh = GetComponentInChildren<Renderer>();
+        audioSource = GetComponent<AudioSource>();
 
         aliveState.walkState = walkState;
 
@@ -59,6 +65,8 @@ public class ZombieStateManager : MonoBehaviour
         currentState = aliveState;
         currentState.EnterState(this);
         rendererMesh.material = matNormal;
+
+        PlayRandomAudio();
     }
 
     // Update is called once per frame
@@ -85,11 +93,13 @@ public class ZombieStateManager : MonoBehaviour
         ctx.Health -= damage;
         GameObject impacto = Instantiate(bulletHole, hitPoint, Quaternion.LookRotation(hitNormal));
         nHits++;
+        audioSource.PlayOneShot(zombieHitClip);
         if (ctx.Health <= 0)
         {
             if (!isDeath)
             {
                 isDeath = true;
+                audioSource.Stop();
                 //score.AddScore(1);
                 //ChangeState(deathState);
             }
@@ -119,5 +129,13 @@ public class ZombieStateManager : MonoBehaviour
             rendererMesh.material = matRed;
         }
 
+    }
+
+    public void PlayRandomAudio()
+    {
+        int randomAudio = Random.Range(0, (audioClipArray.Length)-2);
+        audioSource.clip = audioClipArray[randomAudio];
+        audioSource.loop = true;
+        audioSource.Play();
     }
 }
